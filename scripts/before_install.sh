eb21cccda6e01c3a5cde7ed8af851fa7bf8462e6
tbalestra <theobalestra@me.com>
2022-10-15 21:24:18 -0700

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install v18.10.0
+nvm use v18.10.0
+
 DIR="/home/ubuntu/dwjg-api"
 if [ -d "$DIR" ]; then
 	echo "${DIR} exists"
