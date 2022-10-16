d0d5f7c948632cd3e60be7499cf76e0cba7a35c8
tbalestra <theobalestra@me.com>
2022-10-15 21:39:31 -0700

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -9,7 +9,10 @@ export NVM_DIR="$HOME/.nvm"
 node -v
 npm install
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "preview-dwjg-deploy" ]; then
+echo $DEPLOYMENT_GROUP_NAME
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "preview-dwjg-deploy" ]
+then
 	npm run preview
 else
 	npm run production
