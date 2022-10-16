1ade7de7e7baa0a0d5f4d7af1d3277156e87662f
tbalestra <theobalestra@me.com>
2022-10-15 21:22:06 -0700

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,14 +3,15 @@ sudo chmod -R 777 /home/ubuntu/dwjg-api
 cd /home/ubuntu/dwjg-api
 
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
+# . ~/.nvm/nvm.sh
 # nvm install v18.10.0
-nvm use v18.10.0
+# nvm use v18.10.0
 
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
+node -v
 npm install
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "preview" ]; then
