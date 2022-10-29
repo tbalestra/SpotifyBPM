75d1d37ab6b3a09c5100364260c7953ac618abe7
tbalestra <theobalestra@me.com>
2022-10-29 15:51:43 -0700

    removing by id queries

--- models/business/queries.js
+++ models/business/queries.js
@@ -1,10 +1,4 @@
 import mysql from '../../libs/mysql/index.js';
 
-export const getBusinessById = async (businessId) => {
-	let business = await mysql.awaitQuery('SELECT * FROM business WHERE id = ?', [businessId]);
-
-	return business.length ? business[0] : null;
-};
-
 export const getBusinesses = async (businessIds) => await mysql.awaitQuery('SELECT * FROM business WHERE id IN ?', [businessIds]);
 export const getBusinessesAttributes = async (businessIds) => await mysql.awaitQuery('SELECT * FROM business_attributes WHERE id IN ?', [businessIds]);
--- models/user/queries.js
+++ models/user/queries.js
@@ -1,10 +1,4 @@
 import mysql from '../../libs/mysql/index.js';
 
-export const getUserById = async (userId) => {
-	let user = await mysql.awaitQuery('SELECT * FROM user WHERE id = ?', [userId]);
-
-	return user.length ? user[0] : null;
-};
-
 export const getUsers = async (userIds) => await mysql.awaitQuery('SELECT * FROM user WHERE id IN ?', [userIds]);
 export const getUsersAttributes = async (userIds) => await mysql.awaitQuery('SELECT * FROM user_attributes WHERE id IN ?', [userIds]);
